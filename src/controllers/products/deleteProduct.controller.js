a7d77f828f833496ebd2ab49edf690bf8a911622
Felipe Holanda <93724958+Felipe-Holanda@users.noreply.github.com>
2022-12-20 09:54:09 -0300

    Improvement of code quality

--- src/controllers/products/deleteProduct.controller.js
+++ src/controllers/products/deleteProduct.controller.js
@@ -3,6 +3,6 @@ import deleteProductService from "../../services/products/deleteProduct.service"
 export default async function deleteProductController(req, res) {
     const { id } = req.validatedData;
     const { status } = await deleteProductService(id);
-    return res.status(status).json({});
+    return res.status(status).send();
 
 }
